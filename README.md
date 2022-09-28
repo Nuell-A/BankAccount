d2fa5b32e4cc5ffbea98f581b756b10d2761a9a1
Emanuel Alcala <alcala.arredondo1@gmail.com>
2022-09-27 21:38:55 -0700

    Working BankAccount class so far. Methods implemented and working, but no try... catch handling or anything else.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bank account project. 
+
+- This program will have two superclasses and the CheckingAccount class will inherit from BankAccount. 
+- The superclasses will both contain constructors and the BankAccount class will have methods that deposit, withdraw, summary, along with getters/setters.
+
+- The CheckingAccount class will allow the user to overdraft with a $30 fee.
+
+- Lastly, there will be a test class that will test these two programs. 
---
+++ src/Module1/BankAccount.java
@@ -0,0 +1,75 @@
+package Module1;
+import java.util.Scanner;
+
+public class BankAccount {
+    /*
+     * This class contains attributes, getters/setters, deposit/withdraw, balance, & summary
+     */
+    private String first_name;
+    private String last_name;
+    private int account_id;
+    private double balance;
+
+    public BankAccount() {
+        this.balance = 0;
+    }
+
+    public void setFirstName(String f) {
+        this.first_name = f;
+    }
+
+    public void setLastName(String l) {
+        this.last_name = l;
+    }
+
+    public void setAccountID(int id) {
+        this.account_id = id;
+    }
+
+    public String getFirstName() {
+        return this.first_name;
+    }
+
+    public String getLastName() {
+        return this.last_name;
+    }
+
+    public int getAccountID() {
+        return this.account_id;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void deposit(double money) {
+        this.balance += money;
+    }
+
+    public void withdrawal(double money) {
+        this.balance -= money;
+    }
+
+    public void accountSummary() {
+        System.out.println("Account Summary:");
+        System.out.printf("\n%15s %15s %15s %15s", "ID", "First Name", "Last Name", "Balance");
+
+        System.out.printf("\n%15s %15s %15s %15s", this.account_id, this.first_name, this.last_name, this.balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount ba = new BankAccount();
+
+        ba.deposit(45.30);
+        System.out.println(ba.getBalance());
+
+        ba.withdrawal(10);
+        
+        ba.setAccountID(1);   
+        ba.setFirstName("Emanuel");
+        ba.setLastName("Alcala");
+
+        ba.accountSummary();
+    }
+}
+

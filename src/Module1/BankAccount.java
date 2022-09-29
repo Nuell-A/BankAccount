7da1a121b938d0cd5de2cf274b26be861710d811
Nuell-A <alcala.arredondo1@gmail.com>
2022-09-29 10:27:57 -0700

    Finished project. Basics all implemented, but there is no user inteface or much functionality yet. Test case goes through.

--- src/Module1/BankAccount.java
+++ src/Module1/BankAccount.java
@@ -4,13 +4,13 @@ public class BankAccount {
     /*
      * This class contains attributes, getters/setters, deposit/withdraw, balance, & summary
      */
-    private String first_name;
-    private String last_name;
+    private String first_name, last_name;
     private int account_id;
     private double balance;
 
+    // Initialize balance
     public BankAccount() {
-        this.balance = 0;
+        balance = 0;
     }
 
     public void setFirstName(String f) {
@@ -26,54 +26,37 @@ public class BankAccount {
     }
 
     public String getFirstName() {
-        return this.first_name;
+        return first_name;
     }
 
     public String getLastName() {
-        return this.last_name;
+        return last_name;
     }
 
     public int getAccountID() {
-        return this.account_id;
+        return account_id;
     }
 
     public double getBalance() {
-        return this.balance;
+        return balance;
     }
 
     public void deposit(double money) {
-        this.balance += money;
+        balance += money;
     }
 
     public void withdrawal(double money) {
-        if (money > getBalance()) {
-            CheckingAccount.processWithdrawal(money);
+        balance -= money;
         }
-        else {
-        this.balance -= money;
-        }
-    }
 
     public void accountSummary() {
+        /*
+         * Shows account summary
+         */
         System.out.println("Account Summary:");
         System.out.printf("\n%15s %15s %15s %15s", "ID", "First Name", "Last Name", "Balance");
 
-        System.out.printf("\n%15s %15s %15s %15s", this.account_id, this.first_name, this.last_name, this.balance);
-    }
-
-    public static void main(String[] args) {
-        BankAccount ba = new BankAccount();
-
-        ba.deposit(45.30);
-        System.out.println(ba.getBalance());
-
-        ba.withdrawal(10);
-        
-        ba.setAccountID(1);   
-        ba.setFirstName("Emanuel");
-        ba.setLastName("Alcala");
-
-        ba.accountSummary();
+        System.out.printf("\n%15s %15s %15s %15s", account_id, first_name, last_name, balance);
     }
 }
 
--- src/Module1/CheckingAccount.java
+++ src/Module1/CheckingAccount.java
@@ -1,10 +1,32 @@
 package Module1;
 
-class CheckingAccount extends BankAccount {
-    private double rate;
-    private double fee = 30;
+public class CheckingAccount extends BankAccount {
+    private double rate = 5;
+    private int fee = 30;
 
-    public static void processWithdrawal(double money) {
+    public CheckingAccount() {
+        super();
+    }
+    
+    public String processWithdrawal(double money) {
+        /*
+         * Withdraws money from account, but if balance is
+         * under 0, then a fee is applied and interest is applied.
+         */
+        withdrawal(money);
         
+        if (getBalance() < 0) {
+            withdrawal(fee);
+            return "Fee has been applied (overdraft)\n";
+        }
+        return "Withdrawal complete.\n";
+    }
+
+    public void displayAccount() {
+        /*
+         * Shows account summary with included interest rate.
+         */
+        accountSummary();
+        System.out.println("\nInterest Rate: " + rate);
     }
 }
